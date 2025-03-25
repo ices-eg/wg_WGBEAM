f8fe829b1b82e6b4795ba0f1915ff3e25dc7a1bb
LECOMTE <jean.baptiste.lecomte@ifremer.fr>
2025-03-25 15:03:02 +0100

    fix deprecated purrr function

--- aged_data_analysis/2023_comparison_age_based_index/R/fun/compute_alk.R
+++ aged_data_analysis/2023_comparison_age_based_index/R/fun/compute_alk.R
@@ -1,6 +1,6 @@
 compute_alk <- function(df_datras_hh_ca,
                         age_plus_group = NULL,
-                        index_grouping_var= c("Year")){
+                        index_grouping_var = c("Year")) {
 
   new_age_plus_group_level <- paste0(age_plus_group, "+")
   old_age_plus_group_level <- as.character(age_plus_group)
@@ -8,29 +8,29 @@ compute_alk <- function(df_datras_hh_ca,
   ### for male and female without distinction
   df_alk <- df_datras_hh_ca %>%
     ### make sure that no double rows
-    distinct() %>%
+    dplyr::distinct() %>%
     ### remove age with NA
-    filter(!is.na(Age)) %>%
-    # mutate(LngtClass = as.numeric(as.character(LngtClass)),
-    #        LngtClass = case_when(LngtCode %in% c(".", "0") ~ LngtClass / 10,
-    #                              LngtCode == "1" ~ LngtClass),
-    #        LngtClass = round(LngtClass, digits = 0)) %>%
-  purrr::when(
-    is.null(age_plus_group) ~ .,
-    ~ dplyr::mutate(., Age = ifelse(Age < age_plus_group, Age, age_plus_group))
-  ) %>%
+    dplyr::filter(!is.na(Age))
 
-    ### only select the usefull columns
+  # Conditional mutate based on age_plus_group
+  if (!is.null(age_plus_group)) {
+    df_alk <- df_alk %>%
+      dplyr::mutate(Age = ifelse(Age < age_plus_group, Age, age_plus_group))
+  }
+
+  df_alk <- df_alk %>%
+    ### only select the useful columns
     dplyr::select(LngtClass, Age, CANoAtLngt, Year, all_of(index_grouping_var)) %>%
-    group_by(LngtClass, Age, across(all_of(index_grouping_var))) %>%
+    dplyr::group_by(LngtClass, Age, across(all_of(index_grouping_var))) %>%
     ### sum the number of aged sole per length class
-    summarise(n_fish = sum(CANoAtLngt)) %>%
-    arrange(Age, LngtClass) %>%
-    group_by(LngtClass, across(all_of(index_grouping_var))) %>%
-    mutate(total_n_fish = sum(n_fish),
-           Proportion = n_fish/total_n_fish,
-           Age_f = as_factor(Age),
-           Age_f = fct_recode(Age_f, !!!setNames(old_age_plus_group_level, new_age_plus_group_level)),
-           Age_f = fct_relevel(Age_f, !!new_age_plus_group_level, after = Inf))
+    dplyr::summarise(n_fish = sum(CANoAtLngt)) %>%
+    dplyr::arrange(Age, LngtClass) %>%
+    dplyr::group_by(LngtClass, across(all_of(index_grouping_var))) %>%
+    dplyr::mutate(total_n_fish = sum(n_fish),
+                  Proportion = n_fish / total_n_fish,
+                  Age_f = forcats::as_factor(Age),
+                  Age_f = forcats::fct_recode(Age_f, !!!setNames(old_age_plus_group_level, new_age_plus_group_level)),
+                  Age_f = forcats::fct_relevel(Age_f, !!new_age_plus_group_level, after = Inf))
+
   return(df_alk)
 }
