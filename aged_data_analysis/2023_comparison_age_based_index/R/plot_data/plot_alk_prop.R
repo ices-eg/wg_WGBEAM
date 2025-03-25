62c980b78ffa1c97f51db2814e08b21eb9e9376e
LECOMTE <jean.baptiste.lecomte@ifremer.fr>
2025-03-25 17:13:22 +0100

    update plot scripts for one or more species

--- aged_data_analysis/2023_comparison_age_based_index/R/plot_data/plot_alk_prop.R
+++ aged_data_analysis/2023_comparison_age_based_index/R/plot_data/plot_alk_prop.R
@@ -2,18 +2,26 @@
 ### Plot df_alk_prop_female
 ################################################################################
 
+if(length(index_grouping_var) == 1){
+  facet_formula <- glue("ScientificName ~ {index_grouping_var}")
+} else {
+  if ("ScientificName" %in% index_grouping_var) {
+    index_grouping_var_temp <-  index_grouping_var[index_grouping_var != "ScientificName"]
+    facet_formula <- glue("ScientificName ~ {paste(index_grouping_var_temp, collapse = '+')}")
+  } else {
+    facet_formula <- glue("ScientificName ~ {paste(index_grouping_var, collapse = '+')}")
+  }
+}
+
 plot_alk_prop <- ggplot() +
   geom_bar(data = df_alk_prop_long,
            aes(x = LngtClass, y = Proportion, fill = Age_f ),
            stat = "identity") +
-  facet_grid(as.formula(glue("{index_grouping_var[2]} ~ Year")))  +
+  facet_grid(as.formula(facet_formula))  +
   theme(axis.text.x = element_text(angle = 90)) +
-  scale_fill_discrete(name = "Age") +
-  xlim(0, 30)
+  scale_fill_discrete(name = "Age")
 print(plot_alk_prop)
 
 ggsave(plot = plot_alk_prop,
-       filename = here("figures",
-                       species_name,
-                       glue("plot_alk_prop.{device_figure}")),
+       filename = here(glue("{path_figures}/plot_alk_prop.{device_figure}")),
        device = device_figure)
--- aged_data_analysis/2023_comparison_age_based_index/R/plot_data/plot_length_number.R
+++ aged_data_analysis/2023_comparison_age_based_index/R/plot_data/plot_length_number.R
@@ -6,12 +6,12 @@
 ### histogram plot
 ### point plot
 dfs_hl_year <- df_hh_hl %>%
-  group_by(Year, across(all_of(index_grouping_var))) %>%
+  group_by(Year, ScientificName, across(all_of(index_grouping_var))) %>%
   mutate(n_fish_tot = n()) %>%
-  group_by(Year, LngtClass, across(all_of(index_grouping_var))) %>%
+  group_by(Year, LngtClass, ScientificName, across(all_of(index_grouping_var))) %>%
   summarise(n_fish = n(),
             n_fish_tot = unique(n_fish_tot)) %>%
-  group_by(Year) %>%
+  group_by(Year, ScientificName) %>%
   mutate(Year = as_factor(Year),
          percentage_n_fish = n_fish / n_fish_tot * 100)
 
@@ -20,13 +20,11 @@ plot_hist_length_str_fyear <- ggplot(data = dfs_hl_year,
                                      aes(x = LngtClass,
                                          y = n_fish)) +
   geom_col() +
-  facet_grid( ~ Year)
+  facet_grid( ScientificName ~ Year)
 print(plot_hist_length_str_fyear)
 
 ggsave(plot = plot_hist_length_str_fyear,
-       filename = here("figures",
-                       species_name,
-                       glue("plot_hist_length_str_fyear.{device_figure}")),
+       filename = here(glue("{path_figures}/plot_hist_length_str_fyear.{device_figure}")),
        device = device_figure)
 
 
@@ -36,15 +34,14 @@ plot_length_str <- ggplot(data = df_hh_hl,
   geom_histogram(stat = "count") +
   ylab("Number") +
   xlab("Length class") +
-  scale_fill_discrete(name = "Year")
+  scale_fill_discrete(name = "Year")  +
+  facet_grid( ScientificName ~ .)
 
 print(plot_length_str)  +
   theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
 ggsave(plot = plot_length_str,
-       filename = here("figures",
-                       species_name,
-                       glue("plot_length_str.{device_figure}")),
+       filename = here(glue("{path_figures}/plot_length_str.{device_figure}")),
        device = device_figure)
 
 ###-----------------------------------------------------------------------------
@@ -66,26 +63,24 @@ plot_length_str_line <- ggplot() +
   geom_point(data = dfs_hl_data_year,
              aes(x = LngtClass,
                  y =  percentage_n_fish,
-                 group = Year,
-                 colour = Year)) +
+                 group = Year),
+             colour = "black") +
   geom_line(data = dfs_hl_data_year,
             aes(x = LngtClass,
                 y =  percentage_n_fish,
-                group = Year,
-                colour = Year),
+                group = Year),
+                colour = "black",
             size = 2.5,
-            alpha = 0.5) +
+            alpha = 0.8) +
   ylab("Number percentage") +
   xlab("Length class") +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
-  facet_wrap(as.formula(glue("~ {index_grouping_var[2]}")), ncol = 2)
+  facet_grid(as.formula(glue("ScientificName ~ Country")))
 
 print(plot_length_str_line)
 
 ggsave(plot = plot_length_str_line,
-       filename = here("figures",
-                       species_name,
-                       glue("plot_length_str_line.{device_figure}")),
+       filename = here(glue("{path_figures}/plot_length_str_line.{device_figure}")),
        device = device_figure)
 
 list_plot_hl <- list(plot_hist_length_str_fyear = plot_hist_length_str_fyear,
